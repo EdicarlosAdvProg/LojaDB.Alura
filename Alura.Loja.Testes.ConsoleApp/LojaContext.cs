0f5c6697a83ec1f4668f0a53a7ffd32a3d072621
Edicarlos <edicarlos.mail@gmail.com>
2022-05-26 23:11:41 -0300

    Primeiro uso da ferramenta Entity Framework

---
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class LojaContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=DESKTOP-H77C52F\\SQLEXPRESS;" +
+                "Initial Catalog=LojaDB;User ID=sa;Password=1a5w2s3d;" +
+                "Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;" +
+                "ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
