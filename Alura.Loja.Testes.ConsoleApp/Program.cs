96fce6b4ab18187d9504138a928be65a82fbef85
Edicarlos <edicarlos.mail@gmail.com>
2022-05-27 21:59:49 -0300

    Adicionando classe Compra no projeto e tabela Compra no banco de dados

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -8,8 +8,22 @@ namespace Alura.Loja.Testes.ConsoleApp {
     class Program {
         static void Main(string[] args) {
 
+            var paoFrances = new Produto();
+            paoFrances.Nome = "Pão francês";
+            paoFrances.PrecoUnitario = 0.40;
+            paoFrances.Unidade = "Unidade";
+            paoFrances.Categoria = "Padaria";
 
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
   
+            using (var context=new LojaContext()) {
+                context.Compras.Add(compra);
+                context.SaveChanges();
+            }
+
         }
     }
 }
